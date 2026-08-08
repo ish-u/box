15dc8254f8e025c7870af7f4bc0f539059d94a2d
Anmol Gupta <anmolgupta520@gmail.com>
2026-08-09 02:27:40 +0530

    add send_command

--- web_server/main.py
+++ web_server/main.py
@@ -5,24 +5,25 @@ app = Flask(__name__)
 
 PIPE = "/tmp/pipe"
 
+def send_command(command):
+    fd = os.open(PIPE, os.O_WRONLY)
+    try:
+        os.write(fd, f"{command}\n".encode())
+    finally:
+        os.close(fd)
+
 @app.get("/")
 def index():
     return render_template("index.html")
 
 @app.get("/next")
 def next_sketch():
-    fd = os.open(PIPE, os.O_WRONLY)
-    os.write(fd, b"NEXT\n")
-    os.close(fd)
-
+    send_command("NEXT")
     return {"ok": True}
 
 @app.get("/prev")
 def previous_sketch():
-    fd = os.open(PIPE, os.O_WRONLY)
-    os.write(fd, b"PREV\n")
-    os.close(fd)
-
+    send_command("NEXT")
     return {"ok": True}
 
 if __name__ == "__main__":
