f3183ce9bd68b51a9a14cec0cfbb1091810938f2
Anmol Gupta <anmolgupta520@gmail.com>
2026-08-09 02:13:06 +0530

    open pipe once

--- gpio/main.py
+++ gpio/main.py
@@ -3,19 +3,22 @@ from signal import pause
 
 PIPE = "/tmp/pipe"
 
+pipe = open(PIPE, "w")
+
 next_button = Button(17, bounce_time=0.05)
 prev_button = Button(27, bounce_time=0.05)
 
 
 def send_command(command):
     print(command, flush=True)
-
-    with open(PIPE, "w") as pipe:
-        pipe.write(command + "\n")
-        pipe.flush()
+    pipe.write(command + "\n")
+    pipe.flush()
 
 
 next_button.when_pressed = lambda: send_command("NEXT")
 prev_button.when_pressed = lambda: send_command("PREV")
 
-pause()
+try:
+    pause()
+finally:
+    pipe.close()
