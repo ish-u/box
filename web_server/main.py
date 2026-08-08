3a1ce0254c11711cb1cf835cceb3cc881b61148a
Anmol Gupta <anmolgupta520@gmail.com>
2026-08-08 22:11:02 +0530

    add web_server

---
+++ web_server/main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+PIPE = "/tmp/pipe"
+
+
+@app.get("/next")
+def next_sketch():
+    fd = os.open(PIPE, os.O_WRONLY)
+    os.write(fd, b"NEXT\n")
+    os.close(fd)
+
+    return {"ok": True}
+
+
+@app.get("/prev")
+def previous_sketch():
+    fd = os.open(PIPE, os.O_WRONLY)
+    os.write(fd, b"PREV\n")
+    os.close(fd)
+
+    return {"ok": True}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=1618)
