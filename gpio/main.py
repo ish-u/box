98a196c896c7caaa401ea013ccfe91fda6fa157d
Anmol Gupta <anmolgupta520@gmail.com>
2026-08-09 01:55:22 +0530

    fix bounce time

--- gpio/main.py
+++ gpio/main.py
@@ -1,14 +1,19 @@
 from gpiozero import Button
-import os
+from signal import pause
 
 PIPE = "/tmp/pipe"
 
-next_button = Button(17)
-prev_button = Button(27)
+next_button = Button(17, bounce_time=0.05)
+prev_button = Button(27, bounce_time=0.05)
+
 
 def send_command(command):
+    print(command, flush=True)
+
     with open(PIPE, "w") as pipe:
         pipe.write(command + "\n")
+        pipe.flush()
+
 
 next_button.when_pressed = lambda: send_command("NEXT")
 prev_button.when_pressed = lambda: send_command("PREV")
